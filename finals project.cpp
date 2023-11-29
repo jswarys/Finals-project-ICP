f356dceb3da6f736889c37c631c5eda9161c170d
jswarys <152388540+jswarys@users.noreply.github.com>
2023-11-29 20:54:29 +0800

    finals project.cpp
    The updated version of my code, I added more explanations to each function.

--- finals project.cpp
+++ finals project.cpp
@@ -7,16 +7,22 @@
 
 using namespace std;
 
+// Define a structure to represent a product with a name and price
 struct Product {
-    string name;
-    double price;
+    string name; // This is where we keep the name of the product, like "Shoes" or "Bread".
+    double price; // Here, we store how much the product costs, such as $10.99 or $1.50.
 };
 
-// Function to display available products with prices
+/* this function creates a visual representation, 
+like a catalog, showing all available products with their 
+respective prices in an organized and easy-to-read manner, 
+similar to how a store might display its products on shelves 
+with tags showing names and prices. */
 void displayProducts(const vector<Product>& products) {
-    cout << "Available products:\n";
+    cout << "Available products:\n"; // Tells us we're going to show available products.
     cout << "-------------------------------------\n";
-    cout << setw(20) << "Product" << setw(13) << "Price" << endl;
+    cout << setw(20) << "Product" << setw(13) << "Price" << endl; // Sets up the table headers.
+    // Loop through each product to display its name and price.
     for (size_t i = 0; i < products.size(); ++i) {
         cout << setw(3) << i + 1 << ". " << setw(17) << products[i].name << setw(13) << products[i].price << endl;
     }
@@ -24,9 +30,9 @@ void displayProducts(const vector<Product>& products) {
     cout << "\n";
 }
 
-// function for mode of payment
+// Function to let the user choose a payment method and display a message accordingly.
 void paymentType() {
-    int paymentOption;
+    int paymentOption; // This variable will store the chosen payment option.
     cout << "\nMode of Payment:\n";
     cout << "1. Paymaya\n";
     cout << "2. Credit Card\n";
@@ -36,6 +42,7 @@ void paymentType() {
     cout << "Enter the payment option: ";
     cin >> paymentOption;
 
+    // Check the chosen payment option and display a corresponding message.
     if (paymentOption == 1) {
         cout << "Payment successful using Paymaya.\n";
     }
@@ -55,7 +62,7 @@ void paymentType() {
 }
 
 int main() {
-    // Products available for purchase
+    // A list of available products, each with a name and corresponding price.
     vector<Product> availableProducts = {
         {"Deodorant", 10.0},
         {"Toothpaste", 10.0},
@@ -71,7 +78,7 @@ int main() {
     
     };
 
-    cout << "+------------------------------------+\n";S
+    cout << "+------------------------------------+\n";
     cout << "|  REYES AND URETA'S GROCERY SYSTEM  |\n";
     cout << "+------------------------------------+\n";
     cout << "\n";
@@ -92,7 +99,7 @@ int main() {
     cin >> ldate;
     cout << "\n";
 
-    // Display available products
+    // Display available products than user can choose
     displayProducts(availableProducts);
 
     // Get user's choices
@@ -101,13 +108,15 @@ int main() {
     cin >> productCount;
 
     double totalPrice = 0.0;
+    // Loop to collect user's choices for products
     for (int i = 0; i < productCount; ++i) {
         int productChoice;
         cout << "Enter choice " << i + 1 << " (1-" << availableProducts.size() << "): ";
         cin >> productChoice;
         productChoice--; // Adjust for 0-based indexing
-
+        // Check if the chosen product exists in the available products list
         if (productChoice >= 0 && productChoice < availableProducts.size()) {
+            // Add the price of the selected product to the total price
             totalPrice += availableProducts[productChoice].price;
         }
         else {
@@ -118,13 +127,16 @@ int main() {
     }
 
     // Clear input buffer
-    cin.clear();
+    cin.clear(); // Clears any error flags or unexpected input state.
+    
+    // Ignores any remaining characters in the input buffer up to a newline.
+    // This helps to prevent issues if there are any extra characters left in the input buffer after reading data.
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    // Mode of payment
+    // Declares the options for payment
     paymentType();
 
-    // Generate random reference code
+    // Generates random reference code
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dis(1000000, 9999999);
